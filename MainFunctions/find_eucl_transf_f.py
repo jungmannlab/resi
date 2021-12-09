960e41925be40a60b57af5edadc8099b9655ff03
SusanneReinhardt <susanne.reinhardt97@web.de>
2021-12-09 11:17:48 +0100

    Run find_eucl_transf from main file

--- MainFunctions/find_eucl_transf_f.py
+++ MainFunctions/find_eucl_transf_f.py
@@ -17,155 +17,143 @@ import pandas as pd
 import os
 
 
-plt.close('all')
+def find_eucl_transf_f(folder_path, ch1_files, ch3_files):
+    plt.close('all')
 
-folder_path = r'/Volumes/pool-miblab4/users/reinhardt/z_raw/Resi/211129_double-ext-20nm-2del-6sites-R4-single-site-ca/aligned/Rotation-test/alignment_locs/'
 
-plt.close('all')
+    plt.close('all')
 
-px_size = 130 # nm
+    px_size = 130 # nm
 
-# I will avoid calling them "R1" or "R3" since the data I am loading will be 
-# from R4 imagers imaged in each round
+    # I will avoid calling them "R1" or "R3" since the data I am loading will be 
+    # from R4 imagers imaged in each round
 
-ch1_files = []
-ch3_files = []
+    eucl_tr_params = [] # initialize the list of Euclidean transf parameters
 
-# get number of files assuming there are 4 per origami (R1, R2, yaml and hdf5)
-n_files = int(len(os.listdir(folder_path))/4)
-
-# get all hdf5 file names in a list
-for i in range(n_files):
-
-    ch1_files.append('R1_apicked_ori' + str(i) + '_apicked.hdf5')
-    ch3_files.append('R3_apicked_ori' + str(i) + '_apicked.hdf5')
-
-eucl_tr_params = [] # initialize the list of Euclidean transf parameters
-
-for i, (file1, file3) in enumerate(zip(ch1_files, ch3_files)):
-    
-    print(i)
-    
-    # load the localizations for each channel
-
-    ch1_fulltable = pd.read_hdf(folder_path + file1, key = 'locs')
-    ch1_fulltable['x'] = ch1_fulltable['x']*px_size # convert to nm
-    ch1_fulltable['y'] = ch1_fulltable['y']*px_size
-    
-    ch3_fulltable = pd.read_hdf(folder_path + file3, key = 'locs')
-    ch3_fulltable['x'] = ch3_fulltable['x']*px_size # convert to nm
-    ch3_fulltable['y'] = ch3_fulltable['y']*px_size
-    
-    number_of_R4_sites = ch1_fulltable['group'].max()+1 # usually 6 but we discard the ones lacking one of the channel
-    
-    locsx_ch1 = [] # lists that will have all the cleaned-up localizations for each channel
-    locsy_ch1 = []
-    locsx_ch3 = []
-    locsy_ch3 = []
-    
-    for j in range(number_of_R4_sites): # loop over the localization of each site
+    for i, (file1, file3) in enumerate(zip(ch1_files, ch3_files)):
         
-        locs_ch1 = ch1_fulltable.loc[ch1_fulltable['group'] == j]
-        locs_ch3 = ch3_fulltable.loc[ch3_fulltable['group'] == j]
+        print(i)
         
-        x_ch1 = locs_ch1['x'].to_numpy()
-        y_ch1 = locs_ch1['y'].to_numpy()
+        # load the localizations for each channel
+
+        ch1_fulltable = pd.read_hdf(file1, key = 'locs')
+        ch1_fulltable['x'] = ch1_fulltable['x']*px_size # convert to nm
+        ch1_fulltable['y'] = ch1_fulltable['y']*px_size
         
-        x_ch3 = locs_ch3['x'].to_numpy()
-        y_ch3 = locs_ch3['y'].to_numpy()
+        ch3_fulltable = pd.read_hdf(file3, key = 'locs')
+        ch3_fulltable['x'] = ch3_fulltable['x']*px_size # convert to nm
+        ch3_fulltable['y'] = ch3_fulltable['y']*px_size
         
-        # the eucl tr estimation needs arrays with equal number of elements
-        # therefore we will match the (in general different) number of 
-        # elements in ch1 and ch3
+        number_of_R4_sites = ch1_fulltable['group'].max()+1 # usually 6 but we discard the ones lacking one of the channel
         
-        if np.size(locs_ch1) > np.size(locs_ch3):
+        locsx_ch1 = [] # lists that will have all the cleaned-up localizations for each channel
+        locsy_ch1 = []
+        locsx_ch3 = []
+        locsy_ch3 = []
         
-            size = np.shape(locs_ch3)[0]
-            x_ch1 = np.random.choice(x_ch1, size=size, replace=False, p=None)
-            y_ch1 = np.random.choice(y_ch1, size=size, replace=False, p=None)
+        for j in range(number_of_R4_sites): # loop over the localization of each site
             
-        elif np.size(locs_ch1) < np.size(locs_ch3):
+            locs_ch1 = ch1_fulltable.loc[ch1_fulltable['group'] == j]
+            locs_ch3 = ch3_fulltable.loc[ch3_fulltable['group'] == j]
             
-            size = np.shape(locs_ch1)[0]
-            x_ch3 = np.random.choice(x_ch3, size=size, replace=False, p=None)
-            y_ch3 = np.random.choice(y_ch3, size=size, replace=False, p=None)
-               
-        else:
+            x_ch1 = locs_ch1['x'].to_numpy()
+            y_ch1 = locs_ch1['y'].to_numpy()
             
-            pass
+            x_ch3 = locs_ch3['x'].to_numpy()
+            y_ch3 = locs_ch3['y'].to_numpy()
+            
+            # the eucl tr estimation needs arrays with equal number of elements
+            # therefore we will match the (in general different) number of 
+            # elements in ch1 and ch3
+            
+            if np.size(locs_ch1) > np.size(locs_ch3):
+            
+                size = np.shape(locs_ch3)[0]
+                x_ch1 = np.random.choice(x_ch1, size=size, replace=False, p=None)
+                y_ch1 = np.random.choice(y_ch1, size=size, replace=False, p=None)
+                
+            elif np.size(locs_ch1) < np.size(locs_ch3):
+                
+                size = np.shape(locs_ch1)[0]
+                x_ch3 = np.random.choice(x_ch3, size=size, replace=False, p=None)
+                y_ch3 = np.random.choice(y_ch3, size=size, replace=False, p=None)
+                   
+            else:
+                
+                pass
+            
+            locsx_ch1.append(x_ch1)
+            locsy_ch1.append(y_ch1)
+            locsx_ch3.append(x_ch3)
+            locsy_ch3.append(y_ch3)
+         
+        # put together all localizations for each channel    
+        locsx_ch1 = np.concatenate(locsx_ch1, axis=0)
+        locsy_ch1 = np.concatenate(locsy_ch1, axis=0)
+        locsx_ch3 = np.concatenate(locsx_ch3, axis=0)
+        locsy_ch3 = np.concatenate(locsy_ch3, axis=0)
         
-        locsx_ch1.append(x_ch1)
-        locsy_ch1.append(y_ch1)
-        locsx_ch3.append(x_ch3)
-        locsy_ch3.append(y_ch3)
-     
-    # put together all localizations for each channel    
-    locsx_ch1 = np.concatenate(locsx_ch1, axis=0)
-    locsy_ch1 = np.concatenate(locsy_ch1, axis=0)
-    locsx_ch3 = np.concatenate(locsx_ch3, axis=0)
-    locsy_ch3 = np.concatenate(locsy_ch3, axis=0)
-    
-    # prepare the data in the matrix form
-    N = int(len(locsx_ch1))
-    A = np.concatenate((locsx_ch1, locsy_ch1)).reshape(2, N).T
-    B = np.concatenate((locsx_ch3, locsy_ch3)).reshape(2, N).T
-    
-    # plot the data before eucl transf
-    fig, ax = plt.subplots(2, figsize=(12,12))
-    
-    ax[0].plot(A[:, 0], A[:, 1], 'o', alpha=0.3, label='Channel 1')
-    ax[0].plot(B[:, 0], B[:, 1], 'o', alpha=0.3, label='Channel 2')
-    ax[0].set_aspect('equal')
-    ax[0].set_xlabel('x (nm)')
-    ax[0].set_ylabel('y (nm)')
-    ax[0].legend()
+        # prepare the data in the matrix form
+        N = int(len(locsx_ch1))
+        A = np.concatenate((locsx_ch1, locsy_ch1)).reshape(2, N).T
+        B = np.concatenate((locsx_ch3, locsy_ch3)).reshape(2, N).T
         
-    # estimate best eucl transf
-    transf_estimate = tf.EuclideanTransform()
-    transf_estimate.estimate(B, A)
-    params = transf_estimate.rotation, transf_estimate.translation
-    
-    # transform the data with the opt params    
-    data_est = transf_estimate(B)
-
-    # plot the data after the transformation
-    ax[1].plot(A[:, 0], A[:, 1], 'o', alpha=0.3, label='Channel 1')
-    ax[1].plot(data_est[:, 0], data_est[:, 1], 'o', alpha=0.3, label='Channel 2 - transformed')
-    ax[1].set_aspect('equal')
-    ax[1].set_xlabel('x (nm)')
-    ax[1].set_ylabel('y (nm)')
-    ax[1].legend()
-    
-    plt.tight_layout()
-    
-    try:
-        os.mkdir(folder_path + '/eucl_transf')
-    
-    except OSError:
-        print ("eucl_trasnf folder already exists")
-    
-    plt.savefig(folder_path + '/eucl_transf/' + 'origami' + str(i), format='pdf')
-
-    # save the params in the list
-    eucl_tr_params.append(params)
-   
-# save the final eucl_tr_params list
-
-angle = []
-tx = []
-ty = []
-for i in range(len(eucl_tr_params)):
-    
-    angle.append(eucl_tr_params[i][0])
-    tx.append(eucl_tr_params[i][1][0])
-    ty.append(eucl_tr_params[i][1][1])
-    
-d = {'rotation':angle, 'translation x':tx, 'translation y':ty}
-df = pd.DataFrame(d)
-df.to_csv(folder_path +  r'/eucl_transf/'+ 'eucl_transf_' + 'data.csv', sep='\t', encoding='utf-8')
-df.to_excel(folder_path + r'/eucl_transf/' + 'eucl_transf_' + 'data.xlsx') 
-
+        # plot the data before eucl transf
+        fig, ax = plt.subplots(2, figsize=(12,12))
         
+        ax[0].plot(A[:, 0], A[:, 1], 'o', alpha=0.3, label='Channel 1')
+        ax[0].plot(B[:, 0], B[:, 1], 'o', alpha=0.3, label='Channel 2')
+        ax[0].set_aspect('equal')
+        ax[0].set_xlabel('x (nm)')
+        ax[0].set_ylabel('y (nm)')
+        ax[0].legend()
+            
+        # estimate best eucl transf
+        transf_estimate = tf.EuclideanTransform()
+        transf_estimate.estimate(B, A)
+        params = transf_estimate.rotation, transf_estimate.translation
+        
+        # transform the data with the opt params    
+        data_est = transf_estimate(B)
 
-#TODO: save plots
+        # plot the data after the transformation
+        ax[1].plot(A[:, 0], A[:, 1], 'o', alpha=0.3, label='Channel 1')
+        ax[1].plot(data_est[:, 0], data_est[:, 1], 'o', alpha=0.3, label='Channel 2 - transformed')
+        ax[1].set_aspect('equal')
+        ax[1].set_xlabel('x (nm)')
+        ax[1].set_ylabel('y (nm)')
+        ax[1].legend()
+        
+        plt.tight_layout()
+        
+        try:
+            os.mkdir(folder_path + '/eucl_transf')
+        
+        except OSError:
+            print ("eucl_trasnf folder already exists")
+        
+        plt.savefig(folder_path + '/eucl_transf/' + 'origami' + str(i), format='pdf')
+
+        # save the params in the list
+        eucl_tr_params.append(params)
+       
+    # save the final eucl_tr_params list
+
+    angle = []
+    tx = []
+    ty = []
+    for i in range(len(eucl_tr_params)):
+        
+        angle.append(eucl_tr_params[i][0])
+        tx.append(eucl_tr_params[i][1][0])
+        ty.append(eucl_tr_params[i][1][1])
+        
+    d = {'rotation':angle, 'translation x':tx, 'translation y':ty}
+    df = pd.DataFrame(d)
+    df.to_csv(folder_path +  r'/eucl_transf/'+ 'eucl_transf_' + 'data.csv', sep='\t', encoding='utf-8')
+    df.to_excel(folder_path + r'/eucl_transf/' + 'eucl_transf_' + 'data.xlsx') 
+
+            
+
+    #TODO: save plots
 
---
+++ main_eucl_transf_Clustering.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 10 16:01:03 2020
+
+@author: reinhardt
+"""
+
+import sys
+import numpy as np
+import glob
+import os
+import os.path
+import itertools
+
+
+from MainFunctions.Clusterer_resi_f import clusterer_resi
+from MainFunctions.Cluster_PostProcessing_new_f import postprocessing
+from MainFunctions.Cluster_PostProcessing_new_f import postprocessing_cross
+from MainFunctions.find_eucl_transf_f import find_eucl_transf_f
+
+'''Parameters'''
+'''============================================================================'''
+
+
+'''Please copy and paste the path to the folder containing the data that should be analyzed.'''
+path = r"W:\users\reinhardt\z.software\Git\RESI\RESI\test_files\main_eucl_transf_Clustering"
+
+
+'''Please choose a value for the following two parameters.'''
+colocalization_radius = 25  # (in nm) This is the distance necessary for the Postprocessing script. 
+                            # It is only used for calculating the percentage of molecules that have another molecule within this distance.
+                            # Thus, this is the distance when 2 molecules are considered to be neighbors (no matter if within one protein species or across two species) 
+
+
+'''Please specify which hdf5 files you want to analyze as well as the respective parameters. Please follow the following pattern'''
+
+data = [["R1", "R1_apicked", 4, 50],
+        ["R3", "R3_apicked", 4, 50]]
+
+
+'''
+data = [["Protein1", "filename_base1", radius1, min_cluster_size1, olig_inter_protein_distance1],
+        ["Protein2", "filename_base2", radius2, min_cluster_size2, olig_inter_protein_distance2]]
+
+# Protein1 is an ideally short name for the protein imaged in this file.
+# filename_base1: All hdf5 files that have this string in their filename are considered to be clustered. 
+    # If for example several pick files have to be analyzed the filename_base should only contain the part of the filename that these pick files have in common.
+    # The code will automatically exclude hdf5 files which contain the word "ClusterD" or "coupling" because these are output files of the here performed anlysis. 
+    # Note that every file that should be clustered needs to contain the word "picked". Otherwise it will not be clustered.
+# radius1 is the radius used for Clustering.
+# min_cluster_size1: This is N_min - the minimal number of localizations that a localization cloud has to contain to be considered a true cluster.
+# olig_inter_protein_distance1: This is the step size used for defining oligomers. Two proteins inside a cluster must be reached within steps of this size.
+'''
+
+
+
+
+'''The script'''
+'''============================================================================'''
+
+'''Find euclidian transformation.'''
+'''============================================================================'''
+# This code expects two different channels!
+if len(data) != 2:
+    raise Exception("There must be data from exactly two channels!")
+
+# The transformation will be performed on the R4 sites in channel1 and channel 3
+path_alignment_picks = os.path.join(path,"alignment_picks")
+
+ch13_files = glob.glob(os.path.join(path_alignment_picks, "*.hdf5"))
+ch1_files = sorted(file for file in ch13_files if data[0][1] in file)
+
+# get the respective list for the ch3 files. 
+# Instead of extracting it in the same way from the ch13_files list
+# it will be created from the ch1_files list. If it would be extracted
+# from ch3_files, we might not notice if files in ch3 are missing.... 
+ch3_files = []
+for ch1_file in ch1_files:
+    ch3_file = ch1_file.replace(data[0][1], data[1][1])
+    ch3_files.append(ch3_file)
+    #print("1", ch1_file)
+    #print("3", ch3_file)
+
+find_eucl_transf_f(path, ch1_files, ch3_files)
+
+
+
+"""
+
+def Clusterer_check(path, radius, min_cluster_size, filename_base):
+    Clusterer_filename_extension = "_ClusterD"+str(radius)+"_"+str(min_cluster_size)
+    for file in glob.glob(os.path.join(path, "*.hdf5")): # searches all hdf5 files
+        #print()
+        #print(file)
+        #print("in for loop")
+        #print(type(file))
+        
+        '''
+        if filename_base in file: 
+            print("true1")
+            print(file)
+            
+            if "picked" in file: 
+                print("true2")
+        print()
+        '''
+        
+        if filename_base in file and "picked" in file and Clusterer_filename_extension not in file and "ClusterD" not in file and "coupling" not in file and "_resi_" not in file: 
+           # print("--------------------------------")
+           # print()
+            # check if file is part of the cell to be clustered (filename base in file)
+            # do not apply clusterer to whole cell ("picked" in file)
+            # check that the file itself is not a file produced by the clusterer itself (file does not contain ClusterD6_15 etc.)
+            npz_file = file + "_varsD" + str(radius) + "_" + str(min_cluster_size) + ".npz"
+            if os.path.isfile(npz_file) != True: 
+                # check that the file has not been clustered already
+                print("clusterer starts the file", file)
+                clusterer_resi(file, radius, min_cluster_size)
+                print("clusterer has finished the file", file)
+                print() 
+                
+'''Clusterer'''
+for protein_info in data: # loops over all elements (= lists)  
+    #print("Hallo")
+    radius = protein_info[2]
+    min_cluster_size = protein_info[3]
+    filename_base = protein_info[1]
+    
+    print(protein_info[0])
+    
+    Clusterer_check(path, radius, min_cluster_size, filename_base)
+
+print("clusterer finished")  
+      
+'''Postprocessing '''  
+for protein_info in data:
+
+    
+    protein = protein_info[0]
+    radius = protein_info[2]
+    min_cluster_size = protein_info[3]
+    filename_base = protein_info[1]
+    
+    #print(glob.glob(os.path.join(path, "*.npz")))
+    for file_npz in glob.glob(os.path.join(path, "*.npz")):
+        if filename_base in file_npz:
+            postprocess_hNN_file = file_npz + "_higher_Neighbors_" + protein + ".csv"
+            if os.path.isfile(postprocess_hNN_file) != True:
+    
+                #postprocessing(os.path.join(path,file_npz), colocalization_radius)
+                postprocessing(protein, file_npz, colocalization_radius)
+                print("clusterer has finished the file", file_npz)
+                print()
+
+# cross correlation
+if len(data) > 1:
+    for pair in itertools.combinations(data,2): # pair = all combinations of proteins, each combination stored as a tuple of two elements in the data list
+
+        protein1 = pair[0][0]
+        radius1 = pair[0][2]
+        min_cluster_size1 = pair[0][3]
+        filename_base1 = pair[0][1]
+        
+        protein2 = pair[1][0]
+        radius2 = pair[1][2]
+        min_cluster_size2 = pair[1][3]
+        filename_base2 = pair[1][1]
+        
+        for file_npz1 in glob.glob(os.path.join(path, "*.npz")):
+            if filename_base1 in file_npz1 and "_varsD"+str(radius1)+"_"+str(min_cluster_size1) in file_npz1: # data 1 is from filename_base1 and the corresponding data2 file will be searched automatically
+                file_npz2 = file_npz1.replace(filename_base1, filename_base2)
+                file_npz2 = file_npz2.replace("_varsD"+str(radius1)+"_"+str(min_cluster_size1), "_varsD"+str(radius2)+"_"+str(min_cluster_size2))
+                #print("npz 1", file_npz1)
+                #print("npz 2", file_npz2)
+                
+                resi_file1 = file_npz1.replace("varsD", "resi_")
+                resi_file1 = resi_file1.replace("npz", "hdf5")
+                #print("resi file1", resi_file1)
+                resi_file2 = file_npz2.replace("varsD", "resi_")
+                resi_file2 = resi_file2.replace("npz", "hdf5")
+        
+                postprocess_hNN_file_ex_1 = file_npz1 + "_higher_Neighbors_" + protein1 + "_to_" + protein2 + ".csv"
+                postprocess_hNN_file_ex_2 = file_npz2 + "_higher_Neighbors_" + protein2 + "_to_" + protein1 + ".csv"
+                if os.path.isfile(postprocess_hNN_file_ex_1) != True or os.path.isfile(postprocess_hNN_file_ex_2) != True:
+                    print("do")
+                    #postprocessing(os.path.join(path,file_npz), colocalization_radius)
+                    postprocessing_cross(protein1, protein2, file_npz1, file_npz2, resi_file1, resi_file2, colocalization_radius)
+                    #print("clusterer has finished the file", file_npz)
+                    #print()
+
+print("postprocessing finished")
+"""
