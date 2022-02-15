bd948edb7d2dfd32cd13b79a48135aab012bfe06
SusanneReinhardt <susanne.reinhardt97@web.de>
2022-02-15 10:53:28 +0100

    Fix path to test_files

--- resi_resampling/get_resi_locs_different_K.py
+++ resi_resampling/get_resi_locs_different_K.py
@@ -10,6 +10,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
+
+from os.path import dirname as up
+cwd = os.getcwd()
+wdir = up(cwd)
+os.chdir(wdir)
+
 import tools
 
 plt.close('all')
